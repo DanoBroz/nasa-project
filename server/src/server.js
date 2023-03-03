4d23fcc2cb9a2df8a00606b14982f0d5c5a056f3
Daniel Brož <danobroz66@gmail.com>
2023-03-03 18:25:09 +0100

    get planets prepare

--- server/src/server.js
+++ server/src/server.js
@@ -6,5 +6,5 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}...`);
 });
