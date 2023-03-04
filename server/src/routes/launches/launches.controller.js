b9fc52a654f9c0824647727333c779fc31ae5074
Daniel Brož <danobroz66@gmail.com>
2023-03-04 16:26:50 +0100

    create post launches with post body validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,33 @@
-const { getAllLaunches } = require("../../models/launches.model");
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches());
 }
 
+function httpAddNewLaunch(req, res) {
+    const launch = req.body;
+
+    launch.launchDate = new Date(launch.launchDate);
+
+    if (
+        !launch.mission ||
+        !launch.rocket ||
+        !launch.launchDate ||
+        !launch.destination
+    ) {
+        return res.status(400).json({
+            error: "Missing required launch property",
+        });
+    }
+
+    launch.launchDate.setFullYear(launch.launchDate.getFullYear() + 1);
+
+    addNewLaunch(launch);
+
+    return res.status(201).json(launch);
+}
+
 module.exports = {
     httpGetAllLaunches,
+    httpAddNewLaunch,
 };
