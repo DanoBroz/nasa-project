3f10752d68b7fcc75a525de7a0ac3cf685d97c2d
Daniel Brož <danobroz66@gmail.com>
2023-03-11 14:10:05 +0100

    finding documents

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -47,8 +47,8 @@ function loadPlanetsData() {
     });
 }
 
-function getAllPlanets() {
-    return habitablePlanets;
+async function getAllPlanets() {
+    return await planets.find({});
 }
 
 module.exports = {
