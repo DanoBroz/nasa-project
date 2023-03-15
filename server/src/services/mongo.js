cf46d61055e64cf3d2f5e2fa0ec584d2ed540e5d
Daniel Brož <danobroz66@gmail.com>
2023-03-15 15:17:38 +0100

    updating tests for mongoose

---
+++ server/src/services/mongo.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const MONGO_URL = `mongodb+srv://nasa-api:a3PHvpjgx1qGJL27@nasacluster.x3b4fhu.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connection.once("open", () => {
+    console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+    console.error("MongoDB connection error", err);
+});
+
+async function mongoConnect() {
+    await mongoose.connect(MONGO_URL);
+}
+
+module.exports = {
+    mongoConnect,
+};
