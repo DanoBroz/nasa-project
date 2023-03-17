926c21c221b5ad802a520fa27874192a7300b211
Daniel Brož <danobroz66@gmail.com>
2023-03-17 17:18:26 +0100

    paginating endpoints

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -94,8 +94,11 @@ async function findLaunch(filter) {
     return await launchesDatabase.findOne(filter);
 }
 
-async function getAllLaunches() {
-    return await launchesDatabase.find({}, { _id: 0, __v: 0 });
+async function getAllLaunches(skip, limit) {
+    return await launchesDatabase
+        .find({}, { _id: 0, __v: 0 })
+        .skip(skip)
+        .limit(limit);
 }
 
 async function getLatestFlightNumber() {
