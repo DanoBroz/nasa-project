86edd98e8297b986fe1feba7ed59b47b6cac7715
Daniel Brož <danobroz66@gmail.com>
2023-03-17 06:31:17 +0100

    loading spacex data in Our API

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 const launchesDatabase = require("./launches.mongo");
 const planets = require("./planets.mongo");
 
@@ -19,6 +21,31 @@ const launch = {
 };
 
 saveLaunch(launch);
+
+const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
+
+async function loadLaunchesData() {
+    console.log("Loading launch data...");
+    const response = await axios.post(SPACEX_API_URL, {
+        query: {},
+        options: {
+            populate: [
+                {
+                    path: "rocket",
+                    select: {
+                        name: 1,
+                    },
+                },
+                {
+                    path: "payloads",
+                    select: {
+                        customers: 1,
+                    },
+                },
+            ],
+        },
+    });
+}
 
 async function getAllLaunches() {
     return await launchesDatabase.find({}, { _id: 0, __v: 0 });
@@ -90,6 +117,7 @@ async function abortLaunchById(launchId) {
 }
 
 module.exports = {
+    loadLaunchesData,
     getAllLaunches,
     scheduleNewLaunch,
     existsLaunchWithId,
